f21f639a4455b768726235471d9bbb05528346bf
Daioutzu <54756730+Daioutzu@users.noreply.github.com>
2024-08-31 03:07:28 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace StageSelect
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            harmony.PatchAll();
+        }
+    }
+}
